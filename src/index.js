5edd65792310de4ff2dd348fd337c9a2d6d10947
Matt Riese <riese.matt@gmail.com>
2021-12-14 17:17:18 -0800

    after lesson 6

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getFirestore,
+  collection,
+  getDocs,
+  addDoc,
+  deleteDoc,
+  doc,
+  onSnapshot,
+} from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAlDhrhSPN3TZqX4wKmxmJwd8RpilVBCWc',
+  authDomain: 'fir-9-dojo-61eca.firebaseapp.com',
+  projectId: 'fir-9-dojo-61eca',
+  storageBucket: 'fir-9-dojo-61eca.appspot.com',
+  messagingSenderId: '48868517322',
+  appId: '1:48868517322:web:126b7655f51173097adb33',
+};
+
+// init firebase app
+initializeApp(firebaseConfig);
+
+//init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, 'books');
+
+// get real time collection data
+onSnapshot(colRef, (snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
+  });
+  console.log(books);
+});
+
+//   // get one-time collection data
+// getDocs(colRef)
+// .then((snapshot) => {
+//   let books = [];
+//   snapshot.docs.forEach((doc) => {
+//     books.push({ ...doc.data(), id: doc.id });
+//   });
+//   console.log(books);
+// })
+// .catch((err) => {
+//   console.error(err.message);
+// });
+
+// adding docs
+const addBookForm = document.querySelector('.add');
+addBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  addDoc(colRef, {
+    title: addBookForm.title.value,
+    author: addBookForm.author.value,
+  }).then(() => addBookForm.reset());
+});
+
+// deleting docs
+const deleteBookForm = document.querySelector('.delete');
+deleteBookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  let bookID = deleteBookForm.id.value;
+  const docRef = doc(db, 'books', bookID);
+  deleteDoc(docRef).then(() => deleteBookForm.reset());
+});
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// core node module
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  watch: true,
+};
